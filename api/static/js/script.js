6634dde6a9a66e7472224ab7da6334ca5a500d88
br1 <bruno_mg96@hotmail.com>
2020-07-26 13:05:04 -0500

    function for api calls

--- api/static/js/script.js
+++ api/static/js/script.js
@@ -11,9 +11,50 @@ $(document).ready(() => {
             { data: 'rating' },
             {
                 data: null,
-                defaultContent: '<button class="btn blue-gradient btn-sm">Edit</button>',
+                defaultContent: '<button class="btn blue-gradient btn-sm">Details</button>'
+                                + '<button class="btn aqua-gradient btn-sm">Edit</button>'
+                                + '<button class="btn peach-gradient btn-sm" data-toggle="modal" data-target="#modalDelete">Delete</button>',
                 targets: -1
             }
         ]
     });
 });
+
+$('#btnCreate').click((e) => {
+    e.preventDefault();
+
+    let url = '/api/restaurants/create';
+    let data = {
+        name: $('#name').val(),
+        phone: $('#phone').val(),
+        email: $('#email').val(),
+        street: $('#street').val(),
+        city: $('#city').val(),
+        state: $('#state').val(),
+        lat: $('#lat').val(),
+        lng: $('#lng').val(),
+        site: $('#site').val()    
+    };
+
+    apiCall(url, data);
+});
+
+function format(d) {
+    return 'ID: '+ d.id;
+}
+
+function apiCall(url, data) {
+    $.ajax({
+        type : 'POST',
+        url : url,
+        data: JSON.stringify(data, null, '\t'),
+        contentType: 'application/json;charset=UTF-8',
+        success: (res) => {
+            $('#toastMsg').text(res.message);
+            $('#notify').toast('show');
+        },
+        error: (err) => {
+            console.error(err);
+        }
+    });
+}
